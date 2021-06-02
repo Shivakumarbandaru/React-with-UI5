bf481dff267da95b33bb3bed4748c515a534698b
Shivakumarbandaru <Shivakumarbandaru457@gmail.com>
2021-06-02 20:35:14 +0530

    commit files

--- my-app/src/CBox.js
+++ my-app/src/CBox.js
@@ -1,46 +1,54 @@
 
 import { ComboBox, ComboBoxItem } from '@ui5/webcomponents-react';
+import { BusyIndicator } from '@ui5/webcomponents-react';
 import React, { useState, useEffect } from "react";
 
 
-export default function Combox()
-{
-  const  [loading, setloading] = useState(true);
+export default function Combox() {
+  const [loading, setloading] = useState(true);
   const [Details, setDetails] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("using useeffect");
-      fetch("https://run.mocky.io/v3/a2e1b059-144e-47de-b175-f070e560ab13")
+    fetch("https://run.mocky.io/v3/3203c168-7fa7-435e-9cd7-eb5ec2560dae")
       .then((res) => res.json())
-      .then((result) =>setDetails(result));   
-      setloading(false);
+      .then((result) => setDetails(result));
+    setloading(false);
   }, []);
 
-  
-  return(
+
+  return (
     <>
-     {loading ? <p>Loading....</p> : Details.map((Detail) => {
-       return(
-        <ComboBox
+      {loading ? 
+     <div> <BusyIndicator
+        active
         className=""
-        
-        onChange={function noRefCheck(){}}
-        onInput={function noRefCheck(){}}
-        onSelectionChange={function noRefCheck(){}}
         slot=""
         style={{}}
         tooltip=""
-      >
-      
-        <ComboBoxItem text={Detail.id} />
-        <ComboBoxItem text={Detail.name} />
-        <ComboBoxItem text={Detail.Address} />
-      </ComboBox>
+      /></div> : Details.map((Detail) => {
+        return (
+          <ComboBox
+            className=""
 
-       )
-     })}
-      
-        
-</>
-    );
+            onChange={function noRefCheck() { }}
+            onInput={function noRefCheck() { }}
+            onSelectionChange={function noRefCheck() { }}
+            slot=""
+            style={{}}
+            tooltip=""
+          >
+
+            <ComboBoxItem text={Detail.id} />
+            <ComboBoxItem text={Detail.name} />
+            <ComboBoxItem text={Detail.Address} />
+          </ComboBox>
+
+        )
+      })
+      }
+
+
+    </>
+  );
 }
